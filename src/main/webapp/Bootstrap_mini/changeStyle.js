819eabebf5186b4910ea3d1d98f28843c2c19d78
Massonus <maksym.lithuanian@gmail.com>
2024-02-28 12:29:27 +0200

    full changed color

--- src/main/webapp/Bootstrap_mini/changeStyle.js
+++ src/main/webapp/Bootstrap_mini/changeStyle.js
@@ -2,13 +2,16 @@ function changeStyle() {
     let nav = document.getElementById('navbar');
     let checked = document.getElementById("checker").checked;
     let searchButton = document.getElementById("search-button");
+    let searchInput = document.getElementById("search-input");
 
 
     if (checked) {
         nav.className = "navbar navbar-expand-lg navbar-dark bg-dark ";
         searchButton.className = "search-button-dark";
+        searchInput.className = "search-input-dark";
     } else {
         nav.className = "navbar navbar-expand-lg navbar-light bg-light";
         searchButton.className = "search-button-light";
+        searchInput.className = "search-input-light";
     }
 }
